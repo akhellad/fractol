a6ffa11b566249c3b51c86601e31680a9911960e
Ahmed Khelladi <akhellad@z4r7p7.42lyon.fr>
2023-01-12 18:25:24 +0100

    changes

--- fractol.c
+++ fractol.c
@@ -38,6 +38,8 @@ typedef	struct s_frac {
 	double nx;
 	double ny; 
 	char *name;
+	float Z;
+	int colors;
 }			t_frac;
 
 typedef struct	s_win {
@@ -82,15 +84,19 @@ void	mlxinit(t_win *win)
 
 void	julia(t_win *win)
 {
-	win->frac.x1 = -1 * win->frac.h;
-	win->frac.x2 = 1 * win->frac.h;
-	win->frac.y1 = -1.2 * win->frac.h;
-	win->frac.y2 = 1.2 * win->frac.h;
+	int	color;
+	double	n_color;
+
+	win->frac.x1 = win->frac.nx + (-1 * win->frac.h);
+	win->frac.x2 = win->frac.nx + (1 * win->frac.h);
+	win->frac.y1 = win->frac.ny + (-1.2 * win->frac.h);
+	win->frac.y2 = win->frac.ny + (1.2 * win->frac.h);
 	win->frac.image_x = 700;
 	win->frac.image_y = 700;
 	win->frac.iteration_max = 100 * win->frac.m;
 	win->frac.x = 0;
 	win->frac.y = 0;
+
 
 	win->frac.zoom_x = win->frac.image_x / (win->frac.x2 - win->frac.x1);
 	win->frac.zoom_y = win->frac.image_y / (win->frac.y2 - win->frac.y1);
@@ -101,8 +107,8 @@ void	julia(t_win *win)
 		{
 			win->frac.zx = win->frac.x / win->frac.zoom_x + win->frac.x1;
 			win->frac.zy = win->frac.y / win->frac.zoom_y + win->frac.y1;
-			win->frac.cx = 0.285 * win->frac.p;
-			win->frac.cy = 0.01 * win->frac.p;
+			win->frac.cx = -0.4 * win->frac.p;
+			win->frac.cy = 0.6 * win->frac.p;
 			win->frac.count = 0;
 			while (win->frac.zx*win->frac.zx + win->frac.zy*win->frac.zy < 4 && win->frac.count < win->frac.iteration_max)
 			{
@@ -114,7 +120,12 @@ void	julia(t_win *win)
 					my_mlx_pixel_put(&win->img1, win->frac.x, win->frac.y, 0x00000);
 			}
 			if (win->frac.count != win->frac.iteration_max)
-				my_mlx_pixel_put(&win->img1, win->frac.x, win->frac.y, (((double)win->frac.count - log2(log2((win->frac.zx  * win->frac.zx ) + (win->frac.zy * win->frac.zy))) + 6.0) * 50));
+			{
+				color = win->frac.count * 256 / win->frac.iteration_max;
+				n_color = 0;
+				n_color = win->frac.colors + ((color / 8) << 24 | (color) << 16 | (color / 2) << 8 | 0);
+				my_mlx_pixel_put(&win->img1, win->frac.x, win->frac.y, n_color);
+			}
 			win->frac.y ++;
 		}
 		win->frac.y = 0;
@@ -134,10 +145,13 @@ int		pick_frac(t_frac *frac)
 
 void	mendel(t_win *window)
 {
-	window->frac.x1 = (-2.0 * window->frac.h) + window->frac.pos_x;
-	window->frac.x2 = window->frac.pos_x + (0.6 * window->frac.h);
-	window->frac.y1 = window->frac.pos_y + (-1.2 * window->frac.h );
-	window->frac.y2 = window->frac.pos_y + (1.2 * window->frac.h);
+	int	color;
+	double	n_color;
+
+	window->frac.x1 = (window->frac.nx + (-2.0 * window->frac.h));
+	window->frac.x2 = (window->frac.nx + (0.6 * window->frac.h));
+	window->frac.y1 = (window->frac.ny + (-1.2 * window->frac.h));
+	window->frac.y2 = (window->frac.ny + (1.2 * window->frac.h));
 	window->frac.image_x = window->frac.win_len;
 	window->frac.image_y = window->frac.win_len;
 	window->frac.iteration_max = 30;
@@ -166,8 +180,12 @@ void	mendel(t_win *window)
 					my_mlx_pixel_put(&window->img1, window->frac.x, window->frac.y, 0x00000);
 			}
 			if (window->frac.count != window->frac.iteration_max)
-				my_mlx_pixel_put(&window->img1, window->frac.x, window->frac.y, \
-				(((double)window->frac.count - log2(log2((window->frac.zx  * window->frac.zx) + (window->frac.zy * window->frac.zy))) + 4.0) * 20));
+			{
+				color = window->frac.count * 256 / window->frac.iteration_max;
+				n_color = 0;
+				n_color =  window->frac.colors + ((color / 8) << 24 | (color) << 16 | (color / 2) << 8 | 0);
+				my_mlx_pixel_put(&window->img1, window->frac.x, window->frac.y, n_color);
+			}
 			window->frac.y ++;
 		}
 		window->frac.y = 0;
@@ -180,16 +198,11 @@ void	mendel(t_win *window)
 
 int mouse_hook(int keycode, int x, int y, t_win *window)
 {
-	double	mouse_x_pos;
-	double	mouse_y_pos;
-
-	mouse_x_pos = (x + window->frac.win_len / 2) / window->frac.h;
-	mouse_y_pos = (y + window->frac.win_len / 2) / window->frac.h;	
 	if (keycode == 5)
 	{
-		window->frac.pos_x += 0.174 * mouse_x_pos;
-		window->frac.pos_y += 0.174 * mouse_y_pos;
 		window->frac.h /= 1.1;
+		window->frac.nx += 0.001 * ((x - window->frac.win_len / 2) * (window->frac.h));
+		window->frac.ny += 0.001 * ((y - window->frac.win_len / 2) * (window->frac.h));
 		window->frac.m *= 1.001;
 		if(pick_frac(&window->frac) == 1)
 			mendel(window);
@@ -198,9 +211,10 @@ int mouse_hook(int keycode, int x, int y, t_win *window)
 	}
 	if (keycode == 4)
 	{
-		window->frac.pos_x -= 0.2 * mouse_x_pos;
-		window->frac.pos_y -= 0.2 * mouse_y_pos;		
-		window->frac.h *= 1.2;
+		printf("zoom = %f\n", window->frac.h);
+		window->frac.h *= 1.1;
+		window->frac.nx -= 0.001 * ((x - window->frac.win_len / 2) * (window->frac.h));
+		window->frac.ny -= 0.001 * ((y - window->frac.win_len / 2) * (window->frac.h));		
 		window->frac.m /= 1.001;
 		if(pick_frac(&window->frac) == 1)
 			mendel(window);
@@ -209,8 +223,11 @@ int mouse_hook(int keycode, int x, int y, t_win *window)
 	}
 	if (keycode == 1)
 	{
-		window->frac.nx = x;
-		window->frac.ny = y;
+		window->frac.h = 1;
+		window->frac.nx = 0;
+		window->frac.ny = 0;
+		window->frac.pos_x = 0;
+		window->frac.pos_y = 0;
 		if(pick_frac(&window->frac) == 1)
 			mendel(window);
 		if(pick_frac(&window->frac) == 2)
@@ -221,43 +238,117 @@ int mouse_hook(int keycode, int x, int y, t_win *window)
 
 int	change(int keycode, t_win *win)
 {
+	printf("%d\n", keycode);
+	if (keycode == 31)
+	{
+		win->frac.p /= 1.001;
+		if(pick_frac(&win->frac) == 1)
+			mendel(win);
+		if(pick_frac(&win->frac) == 2)
+			julia(win);
+	}
+	if (keycode == 34)
+	{
+		win->frac.p *= 1.001;
+		if(pick_frac(&win->frac) == 1)
+			mendel(win);
+		if(pick_frac(&win->frac) == 2)
+			julia(win);
+	}
 	if(keycode == 124)
 	{
-	win->frac.p /= 1.001;
-	win->frac.nx += 1;
-	if(pick_frac(&win->frac) == 1)
-		mendel(win);
-	if(pick_frac(&win->frac) == 2)
-		julia(win);
+		win->frac.nx -= 0.1 * win->frac.h ;
+		if(pick_frac(&win->frac) == 1)
+			mendel(win);
+		if(pick_frac(&win->frac) == 2)
+			julia(win);
 	}
 	if (keycode == 123)
 	{
-	win->frac.p *= 1.001;
-	win->frac.nx -= 1;
+	win->frac.nx += 0.1 * win->frac.h;
 	if(pick_frac(&win->frac) == 1)
 		mendel(win);
 	if(pick_frac(&win->frac) == 2)
 		julia(win);
 	}
+	if (keycode == 126)
+	{
+		win->frac.ny -= 0.1 * win->frac.h;
+		if(pick_frac(&win->frac) == 1)
+			mendel(win);
+		if(pick_frac(&win->frac) == 2)
+			julia(win);
+	}
+	if (keycode == 125)
+	{
+		win->frac.ny += 0.1 * win->frac.h;
+		if(pick_frac(&win->frac) == 1)
+			mendel(win);
+		if(pick_frac(&win->frac) == 2)
+			julia(win);
+	}
 	if (keycode == 69)
 	{
-	win->frac.h /= 1.1;
-	if(pick_frac(&win->frac) == 1)
-		mendel(win);
-	if(pick_frac(&win->frac) == 2)
-		julia(win);
+		win->frac.h /= 1.1;
+		win->frac.m *= 1.001;
+		if(pick_frac(&win->frac) == 1)
+			mendel(win);
+		if(pick_frac(&win->frac) == 2)
+			julia(win);
 	}
 	if (keycode == 78)
 	{
 	win->frac.h *= 1.1;
+	win->frac.m *= 1.001;
 	if(pick_frac(&win->frac) == 1)
 		mendel(win);
 	if(pick_frac(&win->frac) == 2)
 		julia(win);
 	}
+	if (keycode == 35)
+	{
+		win->frac.h = 1;
+		win->frac.nx = 0;
+		win->frac.ny = 0;
+		if(pick_frac(&win->frac) == 1)
+			mendel(win);
+		if(pick_frac(&win->frac) == 2)
+			julia(win);
+	}
+	if (keycode == 82)
+	{
+		win->frac.colors = 0;
+		if(pick_frac(&win->frac) == 1)
+			mendel(win);
+		if(pick_frac(&win->frac) == 2)
+			julia(win);
+	}
+	if (keycode == 83)
+	{
+		win->frac.colors = 327700;
+		if(pick_frac(&win->frac) == 1)
+			mendel(win);
+		if(pick_frac(&win->frac) == 2)
+			julia(win);
+	}
+	if (keycode == 84)
+	{
+		win->frac.colors = 4850000;
+		if(pick_frac(&win->frac) == 1)
+			mendel(win);
+		if(pick_frac(&win->frac) == 2)
+			julia(win);
+	}
+	if (keycode == 85)
+	{
+		win->frac.colors = 832100;
+		if(pick_frac(&win->frac) == 1)
+			mendel(win);
+		if(pick_frac(&win->frac) == 2)
+			julia(win);
+	}
 	return 0;
 }
-
 
 int main ()
 {
@@ -268,7 +359,7 @@ int main ()
 	win.frac.m = 1;
 	win.frac.pos_x = 0;
 	win.frac.pos_y = 0;
-	win.frac.name = "mendel";
+	win.frac.name = "julia";
 	win.mlx = mlx_init();
 	win.mlx_win = mlx_new_window(win.mlx, 700, 700, "first window");
 	win.img1.img = mlx_new_image(win.mlx, 700, 700);
